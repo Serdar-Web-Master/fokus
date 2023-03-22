fa61f6f15111a9dce58779a0ae17b2a115650983
Serdar <serdarcholukov@yandex.com>
2023-03-22 21:42:07 +0500

    3

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+
+const malysh__block = document.querySelector('.malysh__block');
+
+
+btn.addEventListener('click',()=>{
+   console.log("da");
+   malysh__block.classList.add("op");
+})
