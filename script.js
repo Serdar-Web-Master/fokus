3b766cfc4688edcf969a5fbfae84cb9b9a3e8e13
Serdar <serdarcholukov@yandex.com>
2023-04-04 21:10:20 +0500

    fav

--- script.js
+++ script.js
@@ -2,11 +2,37 @@ const btn = document.querySelector('.btn');
 
 const malysh__block = document.querySelector('.malysh__block');
 
+let a = 0;
 
-btn.addEventListener('click',()=>{
-   console.log("da");
-   malysh__block.classList.add("op");
+
+
+function counter(params) {
+   a = a + 1
+   console.log(a);
+}
+
+function malyshOp(params) {
+
+   if(a==1) {
+      malysh__block.classList.add("opacity");
    setTimeout(()=>{
-      malysh__block.classList.remove("op");
+      malysh__block.classList.remove("opacity");
+      a = 0 ;
+      console.log("a=" + a);
    },5000)
-})
+   }
+   
+}
+malysh__block.addEventListener("click",malyshOp)
+
+btn.addEventListener("click",counter);
+
+
+// function f1 (){
+//    console.log("f1");
+   
+//    malysh__block.classList.add("opacity");
+//    setTimeout(()=>{
+//       malysh__block.classList.remove("opacity");
+//    },5000)
+// }
