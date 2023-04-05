9fb952de9a9adc1f4841d84580b9eb4ac7602112
Serdar <serdarcholukov@yandex.com>
2023-04-05 12:14:33 +0500

    red

--- script.js
+++ script.js
@@ -2,26 +2,41 @@ const btn = document.querySelector('.btn');
 
 const malysh__block = document.querySelector('.malysh__block');
 
+const point = document.querySelector('.point');
+
+
 let a = 0;
 
 
 
 function counter(params) {
    a = a + 1
+   point.classList.add("red");
    console.log(a);
 }
+
+// function malyshOp(params) {
+
+//    if(a==1) {
+//       malysh__block.classList.add("opacity");
+//    setTimeout(()=>{
+//       malysh__block.classList.remove("opacity");
+//       a = 0 ;
+//       console.log("a=" + a);
+//    },5000)
+//    }  
+// }
 
 function malyshOp(params) {
 
    if(a==1) {
-      malysh__block.classList.add("opacity");
+      malysh__block.classList.toggle("opacity");
    setTimeout(()=>{
-      malysh__block.classList.remove("opacity");
+      point.classList.remove("red");
       a = 0 ;
       console.log("a=" + a);
    },5000)
-   }
-   
+   }  
 }
 malysh__block.addEventListener("click",malyshOp)
 
